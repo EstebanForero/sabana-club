9cce4c3e3b347027e55e131cb6a1fec3e78f9148
Esteban Forero <estebanmff@outlook.com>
2025-01-25 13:55:09 -0500

    register added

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -1,9 +1,58 @@
+import { Button } from '@heroui/button';
+import { Form } from '@heroui/form';
+import { Input } from '@heroui/input';
 import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react';
 
 export const Route = createFileRoute('/register')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/register"!</div>
+
+  const [action, setAction] = useState(null);
+
+  return (
+    <Form
+      className="w-full max-w-xs flex flex-col gap-4"
+      validationBehavior="native"
+      onSubmit={(e) => {
+        e.preventDefault();
+        let data = Object.fromEntries(new FormData(e.currentTarget));
+
+      }}
+    >
+      <Input
+        isRequired
+        errorMessage="Please enter a valid username"
+        label="Username"
+        labelPlacement="outside"
+        name="username"
+        placeholder="Enter your username"
+        type="text"
+      />
+
+      <Input
+        isRequired
+        errorMessage="Please enter a valid email"
+        label="Email"
+        labelPlacement="outside"
+        name="email"
+        placeholder="Enter your email"
+        type="email"
+      />
+      <div className="flex gap-2">
+        <Button color="primary" type="submit">
+          Submit
+        </Button>
+        <Button type="reset" variant="flat">
+          Reset
+        </Button>
+      </div>
+      {action && (
+        <div className="text-small text-default-500">
+          Action: <code>{action}</code>
+        </div>
+      )}
+    </Form>)
 }
