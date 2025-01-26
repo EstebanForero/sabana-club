111190a6dd46e8f38dfd92ff78841b80397ab5bd
Esteban Forero <estebanmff@outlook.com>
2025-01-25 23:34:57 -0500

    User added

--- src/backend/user.ts
+++ src/backend/user.ts
@@ -11,7 +11,6 @@ export async function getAllUsers(): Promise<UserInfo[]> {
   return await ky.get(`${backendUrl}/user/all`).json<UserInfo[]>();
 }
 
-// Solo se puede usar despues de iniciar sesion
 export async function getCurrentUser(): Promise<UserInfo> {
   return await ky.get(`${backendUrl}/user`).json<UserInfo>();
 }
