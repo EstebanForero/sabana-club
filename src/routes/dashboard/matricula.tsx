6795718286a48cf8439d4d29c7b6498054874d00
dmartin110620 <109118261+dmartin110620@users.noreply.github.com>
2025-01-25 19:58:08 -0500

    matricula-update-shadow

--- src/routes/dashboard/matricula.tsx
+++ src/routes/dashboard/matricula.tsx
@@ -16,13 +16,13 @@ function RouteComponent() {
             Elige una membresía
           </h1>
   
-          <div className="grid grid-cols-3 gap-4 gap-[50px]">
-            <div className="text-left">
-              <p className="text-sm text-gray-400 mb-2">
-                Bronze
+          <div className="grid grid-cols-3 gap-4 gap-[25px]">
+            <div className="text-left rounded-lg shadow-gray-800 shadow-lg p-[20px] bg-sky-500/35">
+              <p className="text-sm font-bold mb-4">
+                 Plan Bronze
               </p>
-              <p className='text-sm text-gray-400 mb-2'>
-                Este plan incluye Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores iste veritatis aut. Quidem architecto, quam qui inventore, sunt provident ipsa natus omnis at minima magnam doloribus exercitationem tempora non placeat!
+              <p className='text-sm text-gray-400 mb-7'>
+                Perfecto para iniciarte en el tenis. Accede a la plataforma para revisar tus estadísticas básicas, agendar entrenamientos ocasionales y participar en torneos locales.
               </p>
               <Link
                 to="/dashboard/entrenamientos"
@@ -31,12 +31,12 @@ function RouteComponent() {
                 USD 10$
               </Link>
             </div>
-            <div className="text-left">
-              <p className="text-sm text-gray-400 mb-2">
-                Plata
+            <div className="text-left rounded-lg shadow-gray-800 shadow-lg p-[20px] bg-sky-500/35">
+              <p className="text-sm font-bold mb-4">
+                Plan Plata
               </p>
-              <p className='text-sm text-gray-400 mb-2'>
-                Este plan incluye Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores iste veritatis aut. Quidem architecto, quam qui inventore, sunt provident ipsa natus omnis at minima magnam doloribus exercitationem tempora non placeat!
+              <p className='text-sm text-gray-400 mb-7'>
+                Ideal para jugadores comprometidos. Monitorea tu desempeño con estadísticas detalladas, organiza entrenamientos frecuentes y accede a torneos nacionales.
               </p>
               <Link
                 to="/dashboard/torneos"
@@ -45,12 +45,12 @@ function RouteComponent() {
                 USD 20$
               </Link>
             </div>
-            <div className="text-left">
-              <p className="text-sm text-gray-400 mb-2">
-                Oro
+            <div className="text-left rounded-lg shadow-gray-800 shadow-lg p-[20px] bg-sky-500/35">
+              <p className="text-sm font-bold mb-4">
+                Plan Oro
                 </p>
-              <p className='text-sm text-gray-400 mb-2'>
-                Este plan incluye Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores iste veritatis aut. Quidem architecto, quam qui inventore, sunt provident ipsa natus omnis at minima magnam doloribus exercitationem tempora non placeat!
+              <p className='text-sm text-gray-400 mb-7'>
+                Diseñado para los más competitivos. Disfruta de estadísticas avanzadas, entrenamientos ilimitados y prioridad en la inscripción a torneos internacionales.
               </p>
               <Link
                 to="/dashboard/matricula"
