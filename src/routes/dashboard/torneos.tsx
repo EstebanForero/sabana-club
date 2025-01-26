04088409d8135845b01f60dcf1f66e08542f5176
Felipeceu21 <juanceur@unisabana.edu.co>
2025-01-25 22:18:22 -0500

    elimn score

--- src/routes/dashboard/torneos.tsx
+++ src/routes/dashboard/torneos.tsx
@@ -49,15 +49,6 @@ function RouteComponent() {
         </div>
 
         <div className="mb-4">
-          <label className="block mb-1 font-semibold text-gray-700">Score</label>
-          <input 
-            type="text" 
-            placeholder="Score" 
-            className="w-full p-2 border rounded text-gray-800" 
-          />
-        </div>
-
-        <div className="mb-4">
           <label className="block mb-1 font-semibold text-gray-700">Position</label>
           <input 
             type="text" 
@@ -82,7 +73,6 @@ function RouteComponent() {
                 <tr className="bg-gray-100">
                   <th className="border p-2 text-left text-gray-800">Player Name</th>
                   <th className="border p-2 text-left text-gray-800">Identification</th>
-                  <th className="border p-2 text-left text-gray-800">Score</th>
                   <th className="border p-2 text-left text-gray-800">Position</th>
                 </tr>
               </thead>
