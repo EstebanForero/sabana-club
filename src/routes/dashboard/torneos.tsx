83aa46696302a59f8f80181c07711c81337002d7
Felipeceu21 <juanceur@unisabana.edu.co>
2025-01-25 21:29:19 -0500

    create tou

--- src/routes/dashboard/torneos.tsx
+++ src/routes/dashboard/torneos.tsx
@@ -5,5 +5,20 @@ export const Route = createFileRoute('/dashboard/torneos')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/dashboard/torneos"!</div>
+  return  <div className="p-4 bg-gray-100 space-y-6">
+  <div className="shadow-md p-4 rounded-lg bg-white">
+    <h2 className="text-xl font-bold mb-4">Create Tournament</h2>
+    <div className="flex space-x-4">
+      <input 
+        type="text" 
+        placeholder="Tournament Name" 
+        className="w-full p-2 border rounded" 
+      />
+      <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+        Create
+      </button>
+    </div>
+  </div>
+
+  </div>
 }
