6bbc1a2b2ef440706a47dd166e70829c29c4fb67
Esteban Forero <estebanmff@outlook.com>
2025-01-25 19:30:50 -0500

    Tournament backend functions added

--- src/backend/entities.ts
+++ src/backend/entities.ts
@@ -8,3 +8,14 @@ export type UserCreationInfo = {
   nombre_tipo_identificacion: string;
 };
 
+export type Tournament = {
+  id_torneo: string;
+  nombre: string;
+};
+
+export type UserTournamentRegistration = {
+  id_persona: string;
+  id_torneo: string;
+  puesto: number;
+};
+
