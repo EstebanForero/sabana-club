931df7d2c98688cf177ffac091d979cec6514012
Felipeceu21 <juanceur@unisabana.edu.co>
2025-01-25 21:33:21 -0500

    svsrv tourne

--- src/routes/dashboard/torneos.tsx
+++ src/routes/dashboard/torneos.tsx
@@ -27,7 +27,15 @@ function RouteComponent() {
             <option value="">Select a tournament</option>
           </select>
         </div>
-        
+        <div className="mb-4">
+          <label className="block mb-1 font-semibold">Player Name</label>
+          <input 
+            type="text" 
+            placeholder="Player Name" 
+            className="w-full p-2 border rounded" 
+          />
+        </div>
+
   </div>
 
   </div>
