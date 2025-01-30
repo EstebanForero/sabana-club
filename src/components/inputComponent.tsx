0ad24b839926d4e4440da623828d216a1894fc27
Esteban Forero <estebanmff@outlook.com>
2025-01-29 21:04:59 -0500

    Input component added

---
+++ src/components/inputComponent.tsx
@@ -0,0 +1,50 @@
+
+import React, { useState } from 'react'
+
+type Props = {
+  onChange: (value: string) => void
+  name: string
+  validator?: (value: string) => string | undefined
+  placeholder?: string
+  type?: React.HTMLInputTypeAttribute
+}
+
+const InputComponent = ({ onChange, name, validator, placeholder, type }: Props) => {
+
+  const [errorMessage, setErrorMessage] = useState('')
+  const [value, setValue] = useState('')
+
+  const onInputChange = (value: string) => {
+    if (!validator) {
+      onChange(value)
+      setValue(value)
+      return
+    }
+
+    const errorMessage = validator(value)
+
+    if (!errorMessage) {
+      onChange(value)
+      setValue(value)
+      return
+    }
+
+    setErrorMessage(errorMessage)
+  }
+
+  return (
+    <div>
+      <label htmlFor={name} className='className="text-sm text-gray-300 mb-2"'>
+        {name}
+      </label>
+      <input value={value} onChange={(e) => onInputChange(e.target.value)}
+        className='p-3 bg-gray-800 border border-gray-700 rounded-lg shadow focus:outline-none focus:ring-2 focus:ring-blue-500'
+        placeholder={placeholder}
+        type={type}
+      />
+      <p className='text-red-500'>{errorMessage}</p>
+    </div>
+  )
+}
+
+export default InputComponent
